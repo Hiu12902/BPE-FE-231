e9cc1a9aca0888c93e464c4887bac4518a5ad571
Hien_Nguyen <73571967+ngochienhv@users.noreply.github.com>
2023-03-02 00:13:48 +0700

    BPE-37 feat: change JSON export format (#8)

---
+++ src/core/toolbar/constants/bpmnElement.ts
@@ -0,0 +1,17 @@
+export const TASK_TYPE = {
+  'bpmn:NoneTask': 0,
+  'bpmn:ServiceTask': 1,
+  'bpmn:ManualTask': 2,
+  'bpmn:ScriptTask': 3,
+  'bpmn:BusinessRuleTask': 4,
+  'bpmn:UserTask': 5,
+};
+
+export const EVENT_TYPE = {
+  'bpmn:StartEvent': 0,
+  'bpmn:EndEvent': 1,
+  'bpmn:ImplicitThrowEvent': 2,
+  'bpmn:IntermediateThrowEvent': 3,
+  'bpmn:IntermediateCatchEvent': 4,
+  'bpmn:BoundaryEvent': 5,
+};
