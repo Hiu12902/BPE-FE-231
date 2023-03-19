118d24898f491b250a97b1c7a70d3896b2fea5c1
HienNguyen <ngochien123hv@gmail.com>
2023-03-19 14:55:51 +0700

    [draft] feat: validation

---
+++ src/core/toolbar/helper/ModelGroup/ModelGroup.tsx
@@ -0,0 +1,53 @@
+import { useContext, useEffect } from 'react';
+import { Stack, Text } from '@mantine/core';
+
+import { ModelerContext } from '../../../context/ModelerContext';
+import useGetModelerModules from '../../../hooks/useGetModelerModule';
+import { DEFAULT_SPACING } from '../../constants/size';
+import { IconBpeHistory, IconBpeSave, IconBpeValidate } from '../../utils/icons/Icons';
+import ToolbarIcon from '../ToolbarIcon/ToolbarIcon';
+
+const ModelGroup = () => {
+  const modeler = useContext(ModelerContext);
+  const [linting, eventBus] = useGetModelerModules(modeler, ['linting', 'eventBus']);
+
+  useEffect(() => {
+    //@ts-ignore
+    eventBus?.on('linting.completed', (issues) => {
+      console.log(issues);
+    });
+  }, [eventBus]);
+
+  return (
+    <Stack spacing={DEFAULT_SPACING - 2}>
+      <ToolbarIcon
+        icon={IconBpeSave}
+        label="Save"
+        title="Save Model"
+        orientation="horizontal"
+        size="small"
+      />
+      <ToolbarIcon
+        icon={IconBpeValidate}
+        label="Validate"
+        title="Validate Model"
+        orientation="horizontal"
+        size="small"
+        //@ts-ignore
+        onClick={() => linting?.toggle()}
+      />
+      <ToolbarIcon
+        icon={IconBpeHistory}
+        label="History"
+        title="Open Model's Edit History"
+        orientation="horizontal"
+        size="small"
+      />
+      <Text size="xs" align="center">
+        Model
+      </Text>
+    </Stack>
+  );
+};
+
+export default ModelGroup;
