db5d22ae7b965e8b50087380a999e8799b37f089
Hien_Nguyen <73571967+ngochienhv@users.noreply.github.com>
2023-03-22 21:59:50 +0700

    Bpe 46/feat: bpmn validation (#14)

--- src/core/modeler/BpmnModeler.jsx
+++ src/core/modeler/BpmnModeler.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import 'bpmn-js/dist/assets/diagram-js.css';
 import 'bpmn-font/dist/css/bpmn-embedded.css';
 import BpmnModeler from 'bpmn-js/lib/Modeler';
+import lintModule from 'bpmn-js-bpmnlint';
+import { Linter } from 'bpmnlint';
 import { BpmnPropertiesPanelModule, BpmnPropertiesProviderModule } from 'bpmn-js-properties-panel';
 import TokenSimulationModule from 'bpmn-js-token-simulation';
 import SimulationSupportModule from 'bpmn-js-token-simulation/lib/simulation-support';
@@ -12,14 +14,19 @@ import './BpmnModeler.css';
 import 'bpmn-js-properties-panel/dist/assets/element-templates.css';
 import 'bpmn-js-properties-panel/dist/assets/properties-panel.css';
 import 'bpmn-js-token-simulation/assets/css/bpmn-js-token-simulation.css';
+import 'bpmn-js-bpmnlint/dist/assets/css/bpmn-js-bpmnlint.css';
 
-import { AppShell, Box, Aside, createStyles } from '@mantine/core';
+import { AppShell, Box, Aside, createStyles, Footer } from '@mantine/core';
 import { PaletteNavbar } from '@/core/palette/PaletteNavbar';
 import BpeToolbar from '@/core/toolbar/Toolbar';
 import { ModelerContext } from '@/core/context/ModelerContext';
 import { PRIMARY_COLOR, PROPERTIES_PANEL_WIDTH } from '@/constants/theme/themeConstants';
 import { TOOLBAR_MODE } from '@/constants/toolbar';
 import { ToolbarModeContext } from '@/core/context/ToolbarModeContext';
+import PropertiesProviderModule from '@/core/properties-panel';
+import PropertiesModdleDescripter from '@/core/properties-panel/descriptors/bpeDescriptor';
+import linterConfig from '../../../packed-config';
+import ValidationTerminal from '@/core/validation-terminal';
 
 const useStyles = createStyles((theme) => ({
   main: {
@@ -53,12 +60,11 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-import PropertiesProviderModule from '../properties-panel';
-import PropertiesModdleDescripter from '../properties-panel/descriptors/bpeDescriptor';
-
 const BpeBpmnModeler = () => {
   const [modeler, setModeler] = useState();
   const [toolbarMode, setToolbarMode] = useState(TOOLBAR_MODE.DEFAULT);
+  const [lintingActive, setLintingActive] = useState(false);
+  const [lintingIssues, setLintingIssues] = useState(false);
   const { classes, cx } = useStyles();
 
   useEffect(() => {
@@ -67,6 +73,7 @@ const BpeBpmnModeler = () => {
       propertiesPanel: {
         parent: '#properties',
       },
+      linting: {},
       additionalModules: [
         BpmnPropertiesPanelModule,
         BpmnPropertiesProviderModule,
@@ -74,6 +81,7 @@ const BpeBpmnModeler = () => {
         TokenSimulationModule,
         SimulationSupportModule,
         SimulationBehaviorModule,
+        lintModule,
       ],
       moddleExtensions: {
         bpe: PropertiesModdleDescripter,
@@ -82,6 +90,10 @@ const BpeBpmnModeler = () => {
         bindTo: document,
       },
     });
+
+    const linting = modeler.get('linting');
+    linting.setLinterConfig(linterConfig);
+
     setModeler(modeler);
 
     (async () => {
@@ -94,6 +106,21 @@ const BpeBpmnModeler = () => {
       }
     })();
   }, []);
+
+  useEffect(() => {
+    if (modeler) {
+      const eventBus = modeler.get('eventBus');
+      eventBus?.on('linting.toggle', ({ active }) => {
+        setLintingActive(() => active);
+      });
+
+      //@ts-ignore
+      eventBus?.on('linting.completed', ({ issues }) => {
+        console.log(issues);
+        setLintingIssues(() => Object.values(issues).flat());
+      });
+    }
+  }, [modeler]);
 
   return (
     <ModelerContext.Provider value={modeler}>
@@ -114,6 +141,14 @@ const BpeBpmnModeler = () => {
             [classes.mainSimulation]: toolbarMode === TOOLBAR_MODE.SIMULATING,
           }),
         }}
+        footer={
+          lintingActive ? (
+            <Footer height={140} fixed={false} style={{ bottom: 0, zIndex: 0 }}>
+              <ValidationTerminal issues={lintingIssues} />
+            </Footer>
+          ) : null
+        }
+        styles={{ main: { padding: 0 } }}
       >
         <Box id="canvas" style={{ height: '100%' }} />
       </AppShell>
