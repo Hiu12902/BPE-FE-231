6973e7f8932e356caacd67b6da6d227a44c72554
Hien_Nguyen <73571967+ngochienhv@users.noreply.github.com>
2023-03-28 23:09:21 +0700

    feat: update properties panel and get json (#15)

--- src/core/properties-panel/components/TextArea.tsx
+++ src/core/properties-panel/components/TextArea.tsx
@@ -1,16 +1,16 @@
 //@ts-ignore
-import { TextFieldEntry } from '@bpmn-io/properties-panel';
+import { TextAreaEntry } from '@bpmn-io/properties-panel';
 //@ts-ignore
 import { useService } from 'bpmn-js-properties-panel';
 import { PROPERTY_TYPES } from '../constants/types';
 
-interface ITextfield {
+interface ITextArea {
   element: any;
   id: string;
   label: string;
   property: string;
 }
-const TextField = (props: ITextfield) => {
+const TextArea = (props: ITextArea) => {
   const { element, id, label, property } = props;
 
   const translate = useService('translate');
@@ -22,9 +22,6 @@ const TextField = (props: ITextfield) => {
       case PROPERTY_TYPES.CYCLE_TIME:
       case PROPERTY_TYPES.LINK_CODE:
       case PROPERTY_TYPES.CONDITION:
-      case PROPERTY_TYPES.PERCENTAGE:
-      case PROPERTY_TYPES.DURATION:
-      default:
         return element.businessObject[property] || '';
       case PROPERTY_TYPES.BRANCHING_PROBABILITY: {
         const flow = element.outgoing?.find((flow: any) => flow.id === label);
@@ -38,9 +35,6 @@ const TextField = (props: ITextfield) => {
       case PROPERTY_TYPES.CYCLE_TIME:
       case PROPERTY_TYPES.LINK_CODE:
       case PROPERTY_TYPES.CONDITION:
-      case PROPERTY_TYPES.PERCENTAGE:
-      case PROPERTY_TYPES.DURATION:
-      default:
         modeling.updateProperties(element, {
           [property]: value,
         });
@@ -59,15 +53,16 @@ const TextField = (props: ITextfield) => {
   };
 
   return (
-    <TextFieldEntry
+    <TextAreaEntry
       id={id}
       element={element}
       label={translate(label)}
       getValue={getValue}
       setValue={setValue}
       debounce={debounce}
+      rows={3}
     />
   );
 };
 
-export default TextField;
+export default TextArea;
--- src/core/properties-panel/constants/types.ts
+++ src/core/properties-panel/constants/types.ts
@@ -1,4 +1,10 @@
 export const PROPERTY_TYPES = {
   CYCLE_TIME: 'cycleTime',
   BRANCHING_PROBABILITY: 'branchingProbability',
+  LINK_CODE: 'linkCode',
+  CONDITION: 'condition',
+  PERCENTAGE: 'percentage',
+  DURATION: 'duration',
+  IS_START: 'isStart',
+  CODE: 'code',
 };
