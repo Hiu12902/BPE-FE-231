b10839047c18209683924409935e3337387b3a22
Híu <hieunguyen12902@gmail.com>
2023-11-01 21:53:21 +0700

    [FEAT]: Workspace management layout & components

---
+++ src/components/WorkspaceManagement/WorkspaceManagement.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WorkspaceManagement = () => {
+  return <div>WorkspaceManagement</div>;
+};
+
+export default WorkspaceManagement;
---
+++ src/components/WorkspaceManagement/index.ts
@@ -0,0 +1,4 @@
+export { default as WorkspaceManagement } from './WorkspaceManagement'
+export { default as Members } from './components/Members'
+export { default as Customization } from './components/Customization'
+export { default as Requests } from './components/Requests'
