ea2358fe8ff1a61b4ce32f3a29cd5f70d5442484
Híu <hieunguyen12902@gmail.com>
2024-03-26 18:39:47 +0700

    [FEAT]: Change evaluation result logic

--- src/core/toolbar/helper/DiagramGroup/DiagramGroup.tsx
+++ src/core/toolbar/helper/DiagramGroup/DiagramGroup.tsx
@@ -14,39 +14,48 @@ import {
   Stack,
   Stepper,
   Text,
+  TextInput,
   Transition,
-} from '@mantine/core';
-import { useEffect, useState } from 'react';
-import { batch, useSelector } from 'react-redux';
+} from "@mantine/core";
+import { useEffect, useRef, useState } from "react";
+import { batch, useSelector } from "react-redux";
 //@ts-ignore
-import { is } from 'bpmn-js/lib/util/ModelUtil';
+import { is } from "bpmn-js/lib/util/ModelUtil";
 
-import evaluatedResultApi from '@/api/evaluatedResult';
-import { TOOLBAR_MODE } from '@/constants/toolbar';
-import useGetModelerModules from '@/core/hooks/useGetModelerModule';
-import { DEFAULT_SPACING } from '@/core/toolbar/constants/size';
-import { IconBpeCompare, IconBpeEvaluate, IconBpeSimulate } from '@/core/toolbar/utils/icons/Icons';
-import * as selectors from '@/redux/selectors';
+import evaluatedResultApi from "@/api/evaluatedResult";
+import { TOOLBAR_MODE } from "@/constants/toolbar";
+import useGetModelerModules from "@/core/hooks/useGetModelerModule";
+import { DEFAULT_SPACING } from "@/core/toolbar/constants/size";
+import {
+  IconBpeCompare,
+  IconBpeEvaluate,
+  IconBpeSimulate,
+} from "@/core/toolbar/utils/icons/Icons";
+import * as selectors from "@/redux/selectors";
 import {
   comparingActions,
   evaluatedResultActions,
   lintingActions,
   tabsSliceActions,
   toolSliceActions,
-} from '@/redux/slices';
-import { TabVariant } from '@/redux/slices/tabs';
-import { useAppDispatch } from '@/redux/store';
-import { useForm } from '@mantine/form';
-import { randomId, useClipboard, useHotkeys } from '@mantine/hooks';
-import { filter, findIndex, flatten, keys, map, values } from 'lodash';
-import { TOOLBAR_HOTKEYS } from '@/core/toolbar/constants/hotkeys';
-import ToolbarIcon from '../ToolbarIcon/ToolbarIcon';
-import { getElementForGraph } from './helper/getElementJson';
-import { IModeler } from '@/redux/slices/model';
-import { PRIMARY_COLOR } from '@/constants/theme/themeConstants';
-import useNotification from '@/hooks/useNotification';
+} from "@/redux/slices";
+import { TabVariant } from "@/redux/slices/tabs";
+import { useAppDispatch } from "@/redux/store";
+import { useForm } from "@mantine/form";
+import { randomId, useClipboard, useHotkeys } from "@mantine/hooks";
+import { filter, findIndex, flatten, keys, map, values } from "lodash";
+import { TOOLBAR_HOTKEYS } from "@/core/toolbar/constants/hotkeys";
+import ToolbarIcon from "../ToolbarIcon/ToolbarIcon";
+import { getElementForGraph } from "./helper/getElementJson";
+import { IModeler } from "@/redux/slices/model";
+import { PRIMARY_COLOR } from "@/constants/theme/themeConstants";
+import useNotification from "@/hooks/useNotification";
+import { openConfirmModal } from "@mantine/modals";
+import { projectApi } from "@/api/index";
+import { getActiveTab, getEvaluatedResult } from "@/redux/selectors";
+import { EvaluationResult } from "@/interfaces/evaluatedResult";
 
-const planeSuffix = '_plane';
+const planeSuffix = "_plane";
 
 interface UserInputProps {
   cycleTime: number | undefined;
@@ -63,17 +72,17 @@ interface UserInputForm {
 }
 
 const UserInputFormTitleMap: Record<keyof UserInputForm, string> = {
-  target: 'Target',
-  worst: 'Worst',
+  target: "Target",
+  worst: "Worst",
 };
 
 const UserInputFormLableMap: Record<keyof UserInputProps, string> = {
-  cycleTime: 'Cycle Time',
-  cost: 'Cost',
-  quality: 'Quality',
-  flexibility: 'Flexibility',
-  transparency: 'Transparency',
-  exceptionHandling: 'Exception Handling',
+  cycleTime: "Cycle Time",
+  cost: "Cost",
+  quality: "Quality",
+  flexibility: "Flexibility",
+  transparency: "Transparency",
+  exceptionHandling: "Exception Handling",
 };
 
 interface IItemProps extends IModeler {
@@ -87,7 +96,10 @@ const ModelItem = (props: IItemProps) => {
   return (
     <Card
       shadow="xs"
-      style={{ cursor: 'pointer', border: checked ? `1px solid ${PRIMARY_COLOR[2]}` : undefined }}
+      style={{
+        cursor: "pointer",
+        border: checked ? `1px solid ${PRIMARY_COLOR[2]}` : undefined,
+      }}
       onClick={() => onSelect?.(id)}
     >
       <Group>
@@ -109,12 +121,14 @@ const DiagramGroup = () => {
   const [showAffix, setShowAffix] = useState(false);
   const [activeStep, setActiveStep] = useState(0);
   const [compareModels, setCompareModels] = useState<IModeler[]>([]);
-  const nextStep = () => setActiveStep((current) => (current < 2 ? current + 1 : current));
-  const prevStep = () => setActiveStep((current) => (current > 0 ? current - 1 : current));
+  const nextStep = () =>
+    setActiveStep((current) => (current < 2 ? current + 1 : current));
+  const prevStep = () =>
+    setActiveStep((current) => (current > 0 ? current - 1 : current));
   const [toggleMode, eventBus, linting] = useGetModelerModules([
-    'toggleMode',
-    'eventBus',
-    'linting',
+    "toggleMode",
+    "eventBus",
+    "linting",
   ]);
   const notify = useNotification();
 
@@ -145,7 +159,7 @@ const DiagramGroup = () => {
   const checkUserInput = () => {
     const invalidValue = findIndex(
       flatten(map(values(userInputForm.values), (obj) => values(obj))),
-      (value) => typeof value !== 'number'
+      (value) => typeof value !== "number"
     );
     return invalidValue < 0;
   };
@@ -169,7 +183,10 @@ const DiagramGroup = () => {
           allowNextStepsSelect={false}
           size="sm"
         >
-          <Stepper.Step label="Select models" description="Select 2 exact models">
+          <Stepper.Step
+            label="Select models"
+            description="Select 2 exact models"
+          >
             <Alert style={{ borderLeft: `5px solid ${PRIMARY_COLOR[0]}` }}>
               Hint: only models opened in editor are shown here
             </Alert>
@@ -188,14 +205,16 @@ const DiagramGroup = () => {
                     } else {
                       if (compareModels.length > 1) {
                         notify({
-                          title: 'Sorry',
+                          title: "Sorry",
                           message:
-                            'We only support comparing 2 models currently, please unselect another model and try again.',
-                          type: 'warning',
+                            "We only support comparing 2 models currently, please unselect another model and try again.",
+                          type: "warning",
                         });
                         return;
                       }
-                      const modeler = modelers.find((modeler) => modeler.id === modelerId);
+                      const modeler = modelers.find(
+                        (modeler) => modeler.id === modelerId
+                      );
                       if (modeler) {
                         setCompareModels((modelers) => [...modelers, modeler]);
                       }
@@ -205,16 +224,25 @@ const DiagramGroup = () => {
               ))}
             </SimpleGrid>
           </Stepper.Step>
-          <Stepper.Step label="Input performance level" description="Input your desired values">
+          <Stepper.Step
+            label="Input performance level"
+            description="Input your desired values"
+          >
             <Container>
               <Stack>
                 <Group position="apart">
                   {keys(userInputForm.values).map((key) => (
                     <Stack>
-                      {keys(userInputForm.values[key as keyof UserInputForm]).map((subkey) => (
+                      {keys(
+                        userInputForm.values[key as keyof UserInputForm]
+                      ).map((subkey) => (
                         <NumberInput
-                          label={`${UserInputFormTitleMap[key as keyof UserInputForm]} ${
-                            UserInputFormLableMap[subkey as keyof UserInputProps]
+                          label={`${
+                            UserInputFormTitleMap[key as keyof UserInputForm]
+                          } ${
+                            UserInputFormLableMap[
+                              subkey as keyof UserInputProps
+                            ]
                           }`}
                           {...userInputForm.getInputProps(`${key}.${subkey}`)}
                         />
@@ -232,13 +260,13 @@ const DiagramGroup = () => {
             variant="default"
             onClick={activeStep > 0 ? prevStep : () => setOpenInputModal(false)}
           >
-            {activeStep > 0 ? 'Back' : 'Cancel'}
+            {activeStep > 0 ? "Back" : "Cancel"}
           </Button>
           <Button
             onClick={activeStep > 0 ? batchEvaluate : nextStep}
             disabled={activeStep === 0 && compareModels.length < 2}
           >
-            {activeStep > 0 ? 'Compare' : 'Next step'}
+            {activeStep > 0 ? "Compare" : "Next step"}
           </Button>
         </Group>
       </Modal>
@@ -246,7 +274,7 @@ const DiagramGroup = () => {
   };
 
   const getJsonFromModel = (modeler: any) => {
-    const elementRegistry = modeler.get('elementRegistry');
+    const elementRegistry = modeler.get("elementRegistry");
     const jsonObj = getElementForGraph(elementRegistry);
     clipboard.copy(JSON.stringify(jsonObj));
     return jsonObj;
@@ -256,7 +284,7 @@ const DiagramGroup = () => {
     //@ts-ignore
     const lintRes = await linting?.lint();
     const issues = flatten(values(lintRes));
-    return filter(issues, (issue) => issue.category === 'error');
+    return filter(issues, (issue) => issue.category === "error");
   };
 
   const handleSwitchToSimulation = async () => {
@@ -273,7 +301,7 @@ const DiagramGroup = () => {
 
     const element = context.newSelection[0];
     dispatch(toolSliceActions.setElementSelected(element));
-    if (is(element, 'bpmn:SubProcess') && element.collapsed === true) {
+    if (is(element, "bpmn:SubProcess") && element.collapsed === true) {
       setShowAffix(true);
       return;
     }
@@ -298,60 +326,133 @@ const DiagramGroup = () => {
     });
   };
 
-  const onEvaluateModel = async () => {
-    const errors = await lint();
-    if (errors.length > 0) {
-      notify({
-        title: 'Oops',
-        message: 'Your model has some errors, please try validating it!',
-        type: 'error',
-      });
-      return;
-    }
+  const fileNameRef = useRef<HTMLInputElement>(null);
 
+  const handleSaveResult = async (
+    result: EvaluationResult[],
+    fileName: string
+  ) => {
     try {
-      const result = await evaluatedResultApi.evaluate(getJsonFromModel(modeler));
+      if (
+        !!result &&
+        currentModeler &&
+        currentModeler?.projectId &&
+        currentModeler?.processId &&
+        currentModeler?.id &&
+        fileName
+      ) {
+        const res = await projectApi.saveResult({
+          projectID: currentModeler?.projectId,
+          processID: currentModeler?.processId,
+          version: currentModeler?.id,
+          name: fileName,
+          result: result,
+        });
+
+        if (res) {
+          notify({
+            title: "Success!",
+            message: "Save evaluated result for model successfully!",
+            type: "success",
+          });
+          return true;
+        }
+      }
+    } catch (err) {
+      notify({
+        title: "Oops",
+        message:
+          "An error has occurred while saving your result, please check your model and try again.",
+        type: "error",
+      });
+      return false;
+    }
+  };
+
+  const handleEvaluate = async () => {
+    try {
+      if (!fileNameRef.current?.value) {
+        notify({
+          title: "Error!",
+          message: "Please enter a valid file name!",
+          type: "error",
+        });
+        return;
+      }
+      const fileName = fileNameRef.current?.value;
+      const result = await evaluatedResultApi.evaluate(
+        getJsonFromModel(modeler)
+      );
       if (result) {
         const newId = randomId();
-        batch(() => {
-          dispatch(evaluatedResultActions.setEvaluatedResult({ result: result, id: newId }));
-          dispatch(
-            tabsSliceActions.setTabs({
-              label: `Evaluated Result - ${currentModeler?.name}`,
-              value: 'evaluateResult',
-              variant: TabVariant.RESULT,
-              toolMode: TOOLBAR_MODE.EVALUATING,
-              id: newId,
-              model: currentModeler?.id,
-              projectID: currentModeler?.projectId,
-              processId: currentModeler?.processId,
-            })
-          );
-          dispatch(toolSliceActions.setToolbarMode(TOOLBAR_MODE.EVALUATING));
-        });
+        const save = await handleSaveResult(result, fileName);
+        if (save) {
+          batch(() => {
+            dispatch(
+              evaluatedResultActions.setEvaluatedResult({
+                result: result,
+                id: newId,
+              })
+            );
+            dispatch(
+              tabsSliceActions.setTabs({
+                label: `Evaluated Result - ${currentModeler?.name}`,
+                value: "evaluateResult",
+                variant: TabVariant.RESULT,
+                toolMode: TOOLBAR_MODE.EVALUATING,
+                id: newId,
+                model: currentModeler?.id,
+                projectID: currentModeler?.projectId,
+                processId: currentModeler?.processId,
+              })
+            );
+            dispatch(toolSliceActions.setToolbarMode(TOOLBAR_MODE.EVALUATING));
+          });
+        }
       }
     } catch (error) {
       notify({
-        title: 'Oops',
+        title: "Oops",
         message:
-          'An error has occurred while evaluating your model, please check your model and try again.',
-        type: 'error',
+          "An error has occurred while evaluating your model, please check your model and try again.",
+        type: "error",
       });
       console.error(error);
     }
   };
 
+  const onEvaluateModel = async () => {
+    const errors = await lint();
+    if (errors.length > 0) {
+      notify({
+        title: "Oops",
+        message: "Your model has some errors, please try validating it!",
+        type: "error",
+      });
+      return;
+    }
+
+    openConfirmModal({
+      title: "Save Result",
+      children: (
+        <TextInput placeholder="Name your result file" ref={fileNameRef} />
+      ),
+      labels: { confirm: "Confirm", cancel: "Cancel" },
+      onConfirm: handleEvaluate,
+    });
+  };
+
   const buildComparePayload = (payload: any[]) => {
     const obj: any = {};
 
-    obj['target'] = {
+    obj["target"] = {
       ...userInputForm.values.target,
     };
-    obj['worst'] = {
+    obj["worst"] = {
       ...userInputForm.values.worst,
     };
-    obj['as_is'] = payload[0];
-    obj['to_be'] = payload[1];
+    obj["as_is"] = payload[0];
+    obj["to_be"] = payload[1];
 
     console.log(obj);
 
@@ -362,7 +463,9 @@ const DiagramGroup = () => {
     try {
       const results = await Promise.all(
         compareModels.map(async (modeler) => {
-          return await evaluatedResultApi.evaluate(getJsonFromModel(modeler.modeler));
+          return await evaluatedResultApi.evaluate(
+            getJsonFromModel(modeler.modeler)
+          );
         })
       );
       const flattenResult = flatten(results);
@@ -377,21 +480,28 @@ const DiagramGroup = () => {
           name: res.name,
         }))
       );
-      const res = await evaluatedResultApi.compare(buildComparePayload(payload));
+      const res = await evaluatedResultApi.compare(
+        buildComparePayload(payload)
+      );
       const newId = randomId();
       batch(() => {
-        dispatch(evaluatedResultActions.setEvaluatedResult({ result: flattenResult, id: newId }));
+        dispatch(
+          evaluatedResultActions.setEvaluatedResult({
+            result: flattenResult,
+            id: newId,
+          })
+        );
         dispatch(comparingActions.setCompareResult(res));
         dispatch(
           comparingActions.setDiagrams({
-            toCompareDiagram: modelers[1].id.replace('mantine-', ''),
-            diagramComparedTo: modelers[0].id.replace('mantine-', ''),
+            toCompareDiagram: modelers[1].id.replace("mantine-", ""),
+            diagramComparedTo: modelers[0].id.replace("mantine-", ""),
           })
         );
         dispatch(
           tabsSliceActions.setTabs({
             label: `Comparing - ${currentModeler?.name}`,
-            value: 'evaluateResult',
+            value: "evaluateResult",
             variant: TabVariant.RESULT,
             toolMode: TOOLBAR_MODE.EVALUATING,
             id: newId,
@@ -405,19 +515,20 @@ const DiagramGroup = () => {
     } catch (err) {
       console.error(err);
       notify({
-        title: 'Oops',
-        message: 'Your models has some errors, please check them and try again!',
-        type: 'error',
+        title: "Oops",
+        message:
+          "Your models has some errors, please check them and try again!",
+        type: "error",
       });
     }
   };
 
   useEffect(() => {
     //@ts-ignore
-    eventBus?.on('selection.changed', onSelectElement);
+    eventBus?.on("selection.changed", onSelectElement);
     return () => {
       //@ts-ignore
-      eventBus?.off('selection.changed', onSelectElement);
+      eventBus?.off("selection.changed", onSelectElement);
     };
   }, [eventBus]);
 
--- src/core/toolbar/helper/EvaluationFunctionGroup/EvaluationFunctionGroup.tsx
+++ src/core/toolbar/helper/EvaluationFunctionGroup/EvaluationFunctionGroup.tsx
@@ -1,97 +1,32 @@
-import { DEFAULT_SPACING } from '@/core/toolbar/constants/size';
-import { IconBpeExportLarge, IconBpeSaveLarge } from '@/core/toolbar/utils/icons/Icons';
-import { Group, Stack, TextInput } from '@mantine/core';
-import ToolbarIcon from '../ToolbarIcon/ToolbarIcon';
-import { CSSProperties, useRef } from 'react';
-import { useSelector } from 'react-redux';
-import { getActiveTab, getEvaluatedResult } from '@/redux/selectors';
-import { openConfirmModal } from '@mantine/modals';
-import projectApi from '@/api/project';
-import useNotification from '@/hooks/useNotification';
-import { TabVariant } from '@/redux/slices/tabs';
+import { DEFAULT_SPACING } from "@/core/toolbar/constants/size";
+import { IconBpeExportLarge } from "@/core/toolbar/utils/icons/Icons";
+import { getActiveTab, getEvaluatedResult } from "@/redux/selectors";
+import { Group, Stack } from "@mantine/core";
+import { CSSProperties, useRef } from "react";
+import { useSelector } from "react-redux";
+import ToolbarIcon from "../ToolbarIcon/ToolbarIcon";
 
 const EvaluationFunctionGroup = ({ style }: { style?: CSSProperties }) => {
-  const activeTab = useSelector(getActiveTab);
-  const evaluatedResult = useSelector(getEvaluatedResult)[activeTab?.id as string];
-  const fileNameRef = useRef<HTMLInputElement>(null);
   const downloadLinkRef = useRef<HTMLAnchorElement>(null);
-  const notify = useNotification();
-
-  const handleSaveResult = async () => {
-    try {
-      if (!fileNameRef.current?.value) {
-        notify({
-          title: 'Error!',
-          message: 'Please enter a valid file name!',
-          type: 'error',
-        });
-        return;
-      }
-      if (
-        !!evaluatedResult &&
-        activeTab &&
-        activeTab.projectID &&
-        activeTab.model &&
-        activeTab?.processId
-      ) {
-        console.log({
-          projectID: activeTab?.projectID,
-          version: activeTab?.model,
-          name: fileNameRef.current.value,
-          result: evaluatedResult,
-          processID: activeTab?.processId,
-        });
-
-        const res = await projectApi.saveResult({
-          projectID: activeTab?.projectID,
-          version: activeTab?.model,
-          name: fileNameRef.current.value,
-          result: evaluatedResult,
-          processID: activeTab?.processId,
-        });
-        if (res) {
-          notify({
-            title: 'Success!',
-            message: 'Save evaluated result for model successfully!',
-            type: 'success',
-          });
-        }
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const openSaveResultModal = () => {
-    openConfirmModal({
-      title: 'Save Result',
-      children: <TextInput placeholder="Name your result file" ref={fileNameRef} />,
-      labels: { confirm: 'Confirm', cancel: 'Cancel' },
-      onConfirm: handleSaveResult,
-    });
-  };
+  const activeTab = useSelector(getActiveTab);
+  const evaluatedResult =
+    useSelector(getEvaluatedResult)[activeTab?.id as string];
 
   const encodeResult = () => {
     const encodedData = encodeURIComponent(JSON.stringify(evaluatedResult));
     downloadLinkRef.current?.setAttribute(
-      'href',
-      'data:application/json;charset=UTF-8,' + encodedData
+      "href",
+      "data:application/json;charset=UTF-8," + encodedData
     );
-    downloadLinkRef.current?.setAttribute('download', `${activeTab?.model}.json`);
+    downloadLinkRef.current?.setAttribute(
+      "download",
+      `${activeTab?.model}.json`
+    );
   };
 
   return (
     <Stack spacing={DEFAULT_SPACING} style={style}>
       <Group>
-        <ToolbarIcon
-          icon={IconBpeSaveLarge}
-          label="Save Result"
-          title="Save evaluated result"
-          orientation="vertical"
-          size="large"
-          onClick={openSaveResultModal}
-          disabled={activeTab?.isCompare}
-        />
         <ToolbarIcon
           icon={IconBpeExportLarge}
           label="Export Result"
